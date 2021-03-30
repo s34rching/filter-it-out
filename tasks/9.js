25ba5adeae003f2c237926fea531e462a0d0f858
s34rching <ivan.katsenya@gmail.com>
2021-03-30 13:35:57 +0300

    Tweak task 9 description

--- tasks/9.js
+++ tasks/9.js
@@ -1,4 +1,4 @@
 // Task 9:
-// Create a function which returns the most frequently used word in subcategories 'ruNames'
+// Create a function which returns the most frequently used words in subcategories 'ruName'
 // with length more than 5 characters
 //
