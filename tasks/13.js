b962515f404194d7eff7b3257a32f96292e014ee
s34rching <ivan.katsenya@gmail.com>
2021-04-08 20:56:46 +0300

    Fix typo

--- tasks/13.js
+++ tasks/13.js
@@ -1,5 +1,5 @@
 // Task 13:
 // Create function which returns the latest category of given section.
-// Append property 'receiveAt' to returning category
+// Append property 'receivedAt' to returning category
 // Property should have value of current time in format "Sunday, February 14th 2010, 3:25:50 pm"
 //
